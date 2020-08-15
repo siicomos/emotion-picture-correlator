6e295cbe3e499bc7da519d24f31ee4abe84226b5
YolandaMM <50937747+YolandaMM@users.noreply.github.com>
2020-08-14 22:33:05 -0700

    update front end

--- src/pages/about-page/About.js
+++ src/pages/about-page/About.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 class About extends Component {
     render(){
         return (
-            <div>
-                <p>Test in About Page</p>
+            <div id='about-us-id'>
+                <h2 className="about-h2" class='text-center'>Motivation</h2>
+                <p class='text-center'> We are using the real-time facial images from user to detect the emotions.
+                According to the emotions, generate the corresponding .gif images.</p>
+                <h2 class='text-center'>Algorithm</h2>
+                <p class='text-center'></p>
+                <h2 className="about-h2" class='text-center'>Team Members</h2>
+                <p class='text-center'>Kang Ling</p>
+                <p class='text-center'>Mingjun Lee</p>
+                <p class='text-center'>Yiwei Ma</p>
             </div>
         );
     } 
--- src/pages/home-page/Home.js
+++ src/pages/home-page/Home.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import { Row, Col } from 'react-bootstrap';
+import './Home.css';
 
 class Home extends Component{
     render(){
         return (
-            <div>
-                <p>Test in Home page</p>
-            </div>
+            <body>
+                <div className='home-con'>
+                    <div>
+                        <Image src={require('../../images/3.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/19.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/47.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                    </div>
+                    <div>
+                        <Image src={require('../../images/6.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/75.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/85.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                    </div>
+                    <div>
+                        <Image src={require('../../images/68.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/23.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                        <Image src={require('../../images/26.gif')} 
+                                width={380}
+                                height={230}
+                                rounded className='home-img'/>
+                    </div>
+                </div>
+            </body>
         );
     }
 

34db4ea6cd017994e52739444c39c48bae7ff0b8
Liam <liam.ling@motorolasolutions.com>
2020-08-17 05:55:18 -0700

    remove some descriptions

--- src/pages/about-page/About.js
+++ src/pages/about-page/About.js
@@ -4,11 +4,7 @@ class About extends Component {
     render(){
         return (
             <div id='about-us-id'>
-                <h2 className="about-h2" class='text-center'>Motivation</h2>
-                <p class='text-center'> We are using the real-time facial images from user to detect the emotions.
-                According to the emotions, generate the corresponding .gif images.</p>
-                <h2 class='text-center'>Algorithm</h2>
-                <p class='text-center'></p>
+            
                 <h2 className="about-h2" class='text-center'>Team Members</h2>
                 <p class='text-center'>Kang Ling</p>
                 <p class='text-center'>Mingjun Lee</p>

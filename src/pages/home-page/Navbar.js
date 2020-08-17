c06d36d7b3e710cc45464b09289ba515fe4b9ec5
Liam <liam.ling@motorolasolutions.com>
2020-08-17 05:56:36 -0700

    fix nav bar

--- src/pages/home-page/Navbar.js
+++ src/pages/home-page/Navbar.js
@@ -9,20 +9,22 @@ class NavBar extends Component{
             <div id='nav-id'>
                 <Navbar collapseOnSelect bg='light' expand='lg'>
                     <Navbar.Brand className="nav-brand">
-                        Emoji Search Engine
+                        Emoji & Meme Search Engine
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Nav>
-                        <Nav.Link className="nav-item" href='/'>
-                            Home
-                        </Nav.Link>
-                        <Nav.Link className="nav-item" href='/webcam'>
-                            Explore Emoji
-                        </Nav.Link>
-                        <Nav.Link className="nav-item" href='about'>
-                            About us
-                        </Nav.Link>
-                    </Nav>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav>
+                            <Nav.Link className="nav-item" href='/'>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link className="nav-item" href='/webcam'>
+                                Explore Emoji
+                            </Nav.Link>
+                            <Nav.Link className="nav-item" href='about'>
+                                About us
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
             </div>
         );
